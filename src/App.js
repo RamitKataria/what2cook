0cc4a43bb61f6a045dc00bda7a5b25499303b705
Khammy (Kate) Saychaleun <khammysaychalern@gmail.com>
2021-11-14 18:27:22 -0800

    fixed React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -3,47 +3,20 @@ import {BrowserRouter as Router, Route,Routes, Link} from "react-router-dom";
 import AboutPage from "./components/about-page";
 import HomePage from "./components/home-page";
 
-import Logo from "./resources/logo.svg"
+
 
 export default function App() {
   const MyPaths = [
-    {name: "Home", path: "/", component: HomePage},
-    {name: "About Us", path: "/about-page", component: AboutPage}
+    {name: "Home", path: "/", component: <HomePage/>},
+    {name: "About Us", path: "/about-page", component: <AboutPage/>}
   ]
 
   return (
     <Router>
-
-      <header>
-      <ol style={{ textDecoration: "none", display: "flex", flex: "row", margin: 0}}>
-
-        <img
-          style={{ width: "100px", margin: "0", padding: "0.1rem"}}
-          src={Logo}
-          alt="What2Cook"/>
-
-          {MyPaths.map((e) => {
-            return (
-                <nav class="d-flex flex-row m-3">
-                  <Link
-                    to={e.path}
-                    style={{ textDecoration: "none", fontSize: "1rem", margin: "3rem" }}
-                  >
-                    {e.name}
-                  </Link>
-                </nav>
-            );
-          })}
-        </ol>
-      </header>
-
-
-      {MyPaths.map((e) => (
-        <Routes>
-        <Route path={e.path} component={e.component} />
-        </Routes>
-      ))}
-      {/* <HomePage/> */}
+      <Routes>
+        <Route exact path="/about-page" element={<AboutPage/>}/>
+        <Route path="/" element={<HomePage/>} /> 
+      </Routes>
     </Router>
   );
 }
--- src/components/home-page.js
+++ src/components/home-page.js
@@ -4,12 +4,13 @@ import Grid from '@mui/material/Grid';
 import MainCard from "./cards/card";
 import ShoppingList from "./cards/shopping-list";
 import RecipeList from "./cards/recipeList";
+import Header from "./header";
 
 export default function HomePage() {
 
 return (
     <Fragment>
-
+    <Header/>
     <Grid container spacing={2} sx={{display: 'flex', justifyContent: 'center', alignItems:"flex-center"}}>
     <ShoppingList/>
     <MainCard/>
