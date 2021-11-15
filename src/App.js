57958218ad554c363f3b6e5ef776f756ad0be89d
Khammy (Kate) Saychaleun <khammysaychalern@gmail.com>
2021-11-14 16:51:35 -0800

    implement React-Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route,Routes, Link} from "react-router-dom";
 
 import AboutPage from "./components/about-page";
-// import HomePage from "./components/home-page";
+import HomePage from "./components/home-page";
 
 export default function App() {
-  // const MyPaths = [
-  //   {name: "Home", path: "/home-page", component: HomePage},
-  //   {name: "About Us", path: "/about-page", component: AboutPage}
-  // ]
+  const MyPaths = [
+    {name: "Home", path: "/home-page", component: HomePage},
+    {name: "About Us", path: "/about-page", component: AboutPage}
+  ]
 
   return (
-    <div>
-    {/* <Router>
-      {MyPaths.map()}
-    </Router> */}
-   {/* <HomePage/> */}
-      <AboutPage/>
-    </div>
+    <Router>
+      {MyPaths.map((e) => (
+        <Routes>
+          <Route path={e.path} componene={e.component}/>
+        </Routes>
+       
+      ))}
+
+      <footer>
+      <ol class="d-flex flex-row m-0" style={{ textDecoration: "none" }}>
+          {MyPaths.map((e) => {
+            return (
+              <nav class="d-flex flex-row m-3">
+                <Link
+                  to={e.path}
+                  style={{ textDecoration: "none", fontSize: "small" }}
+                >
+                  {e.name}
+                </Link>
+              </nav>
+            );
+          })}
+        </ol>
+      </footer>
+    </Router>
   );
 }
