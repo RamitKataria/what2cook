26ddf3d0b3fbd51311c659ae04a8c26738ccb8e0
Khammy (Kate) Saychaleun <khammysaychalern@gmail.com>
2021-11-14 19:52:28 -0800

    fix bug

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -14,7 +14,7 @@ return (
         <CardMedia
           component="img"
           height="140"
-          image="https://images.unsplash.com/photo-1559466273-d95e72debaf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" //image
+          image="https://images.unsplash.com/photo-1559466273-d95e72debaf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80"
           alt="suggested recipe"
         />
         <CardContent>
