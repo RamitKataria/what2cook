fa125d2ec9c4c8207ffdb2a3b227c5264c58a3ce
Khammy (Kate) Saychaleun <khammysaychalern@gmail.com>
2021-11-14 17:27:09 -0800

    add logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route,Routes, Link} from "react-router-dom";
 import AboutPage from "./components/about-page";
 import HomePage from "./components/home-page";
 
+import Logo from "./resources/logo.svg"
+
 export default function App() {
   const MyPaths = [
     {name: "Home", path: "/home-page", component: HomePage},
@@ -13,22 +15,29 @@ export default function App() {
     <Router>
       {MyPaths.map((e) => (
         <Routes>
-          <Route path={e.path} componene={e.component}/>
+          <Route path={e.path} component={e.component}/>
         </Routes>
       ))}
 
       <footer>
       <ol style={{ textDecoration: "none", display: "flex", flex: "row", margin: 0}}>
+
+      <img
+                    style={{ width: "100px", margin: "0", padding: "0.1rem"}}
+                    src={Logo}
+                    alt="What2Cook"
+                  />
+
           {MyPaths.map((e) => {
             return (
-              <nav class="d-flex flex-row m-3">
-                <Link
-                  to={e.path}
-                  style={{ textDecoration: "none", fontSize: "small" }}
-                >
-                  {e.name}
-                </Link>
-              </nav>
+                <nav class="d-flex flex-row m-3">
+                  <Link
+                    to={e.path}
+                    style={{ textDecoration: "none", fontSize: "1rem", margin: "3rem" }}
+                  >
+                    {e.name}
+                  </Link>
+                </nav>
             );
           })}
         </ol>
